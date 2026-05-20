bee6834c16a3cc7125c390c514d4b864d1e37444
WilliamLima02 <william0lima@icloud.com>
2026-05-20 02:55:49 +0100

    Initial commit: Estrutura inicial do projeto configurada com PostgreSQL, CORS e Camadas

---
+++ src/main/java/com/isptec/economiahistoriaapi/EconomiaHistoriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.isptec.economiahistoriaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomiaHistoriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomiaHistoriaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isptec/economiahistoriaapi/EconomiaHistoriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isptec.economiahistoriaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomiaHistoriaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
